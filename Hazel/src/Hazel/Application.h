98288d2d4664dc66d0521b1803b25292dbb0a1ef
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-06 20:47:34 +0530

    Added OpenGL buffer abstraction.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -9,8 +9,8 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
-// TODO - Temp
 #include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
 
 
 namespace Hazel
@@ -42,8 +42,10 @@ namespace Hazel
 
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 	private:
 		static Application* s_Instance;
