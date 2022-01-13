7b1ead54cff11109154473b91c654e2eaacb8425
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-13 23:40:26 +0530

    Added orthographic camera.

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <glm/glm.hpp>
 
 namespace Hazel
 {
@@ -11,8 +12,10 @@ namespace Hazel
 		Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		~Shader();
 
-		void  Bind() const;
-		void  UnBind() const;
+		void Bind() const;
+		void UnBind() const;
+
+		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 
 	private:
 		uint32_t m_RendererID;
