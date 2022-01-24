145b0cd71dafb660eb066d06480323e0e346a6c7
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-25 01:55:36 +0530

    Added Hazel::Scope and Hazel::Ref definitions
    Refactored smart pointers to Hazel types

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <memory>
+
+
 #ifdef HZ_PLATFORM_WINDOWS
 
 	#if HZ_DYNAMIC_LINK
@@ -25,15 +28,33 @@
 #endif // HZ_DEBUG
 
 
+
 #ifdef HZ_ENABLE_ASSERTS
+
 	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+
 #else // HZ_ENABLE_ASSERTS
+
 	#define HZ_ASSERT(x, ...)
 	#define HZ_CORE_ASSERT(x, ...)
+
 #endif // HZ_ENABLE_ASSERTS
+
 
 
 #define BIT(x) (1 << x)
 
-#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1) 
+#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+
+namespace Hazel
+{
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -22,21 +22,33 @@ public:
 			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
-		std::shared_ptr<Hazel::VertexBuffer> vertexBuffer;
+
+
+		Hazel::Ref<Hazel::VertexBuffer> vertexBuffer;
+
 		vertexBuffer.reset(Hazel::VertexBuffer::Create(vertices, sizeof(vertices)));
+
 		Hazel::BufferLayout layout = {
 			{ Hazel::ShaderDataType::Float3, "a_Position" },
 			{ Hazel::ShaderDataType::Float4, "a_Color" }
 		};
+
 		vertexBuffer->SetLayout(layout);
+
 		m_VertexArray->AddVertexBuffer(vertexBuffer);
 
+
+
 		uint32_t indices[3] = { 0, 1, 2 };
-		std::shared_ptr<Hazel::IndexBuffer> indexBuffer;
+
+		Hazel::Ref<Hazel::IndexBuffer> indexBuffer;
+
 		indexBuffer.reset(Hazel::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
 		m_SquareVA.reset(Hazel::VertexArray::Create());
+
 
 		float squareVertices[3 * 4] = {
 			-0.5f, -0.5f, 0.0f,
@@ -45,17 +57,28 @@ public:
 			-0.5f,  0.5f, 0.0f
 		};
 
-		std::shared_ptr<Hazel::VertexBuffer> squareVB;
+
+		Hazel::Ref<Hazel::VertexBuffer> squareVB;
+
 		squareVB.reset(Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+
 		squareVB->SetLayout({
 			{ Hazel::ShaderDataType::Float3, "a_Position" }
 			});
+
 		m_SquareVA->AddVertexBuffer(squareVB);
 
+
+
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-		std::shared_ptr<Hazel::IndexBuffer> squareIB;
+
+		Hazel::Ref<Hazel::IndexBuffer> squareIB;
+
 		squareIB.reset(Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+
 		m_SquareVA->SetIndexBuffer(squareIB);
+
+
 
 		std::string vertexSrc = R"(
 			#version 330 core
@@ -77,6 +100,8 @@ public:
 			}
 		)";
 
+
+
 		std::string fragmentSrc = R"(
 			#version 330 core
 			
@@ -92,7 +117,10 @@ public:
 			}
 		)";
 
+
+
 		m_Shader.reset(Hazel::Shader::Create(vertexSrc, fragmentSrc));
+
 
 		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
@@ -111,6 +139,8 @@ public:
 			}
 		)";
 
+
+
 		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
@@ -125,6 +155,8 @@ public:
 				color = vec4(u_Color, 1.0);
 			}
 		)";
+
+
 
 		m_FlatColorShader.reset(Hazel::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
@@ -186,11 +218,11 @@ public:
 	}
 
 private:
-	std::shared_ptr<Hazel::Shader> m_Shader;
-	std::shared_ptr<Hazel::VertexArray> m_VertexArray;
+	Hazel::Ref<Hazel::Shader> m_Shader;
+	Hazel::Ref<Hazel::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Hazel::Shader> m_FlatColorShader;
-	std::shared_ptr<Hazel::VertexArray> m_SquareVA;
+	Hazel::Ref<Hazel::Shader> m_FlatColorShader;
+	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 
 	Hazel::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
