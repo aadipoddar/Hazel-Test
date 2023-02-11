ded14ee8903262049cccf538a322d6662480833d
Aadi Poddar <aadipoddarmail@gmail.com>
2023-02-11 09:42:29 +0530

    Added basic Framebuffer class

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,7 +2,7 @@
 
 // For use by Hazel Applications
 
-#include "Hazel/Core/Core.h"
+#include "Hazel/Core/Base.h"
 
 #include "Hazel/Core/Application.h"
 #include "Hazel/Core/Layer.h"
@@ -24,6 +24,7 @@
 
 #include "Hazel/Renderer/Buffer.h"
 #include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Framebuffer.h"
 #include "Hazel/Renderer/Texture.h"
 #include "Hazel/Renderer/VertexArray.h"
 
