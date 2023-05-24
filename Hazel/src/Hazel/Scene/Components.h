7102c5fc0f64bbf9fc1f1e5b3cf9a923b71f6ece
Aadi Poddar <aadipoddarmail@gmail.com>
2023-05-24 20:59:59 +0530

    Added SceneCamera + camera resizing support

--- Hazel/src/Hazel/Scene/Components.h
+++ Hazel/src/Hazel/Scene/Components.h
@@ -2,7 +2,7 @@
 
 #include <glm/glm.hpp>
 
-#include "Hazel/Renderer/Camera.h"
+#include "SceneCamera.h"
 
 
 namespace Hazel
@@ -49,15 +49,12 @@ namespace Hazel
 
 	struct CameraComponent
 	{
-		Hazel::Camera Camera;
+		SceneCamera Camera;
 		bool Primary = true; // TODO: This is temporary
+		bool FixedAspectRatio = false;
 
 		CameraComponent() = default;
 		CameraComponent(const CameraComponent&) = default;
-		CameraComponent(const glm::mat4& projection)
-			: Camera(projection)
-		{
-		}
 	};
 
 }
