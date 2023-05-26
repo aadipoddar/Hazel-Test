801ca44e420fa6fe315e9d3a3ce53b917c66674a
Aadi Poddar <aadipoddarmail@gmail.com>
2023-05-26 23:32:52 +0530

    Added Camera Component UI to Properties panel

--- Hazel/src/Hazel/Core/Base.h
+++ Hazel/src/Hazel/Core/Base.h
@@ -18,25 +18,17 @@
 	#define HZ_DEBUGBREAK()
 #endif // HZ_DEBUG
 
-
-
-#ifdef HZ_ENABLE_ASSERTS
-
-	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-
-#else // HZ_ENABLE_ASSERTS
-
-	#define HZ_ASSERT(x, ...)
-	#define HZ_CORE_ASSERT(x, ...)
-
-#endif // HZ_ENABLE_ASSERTS
-
-
+#define HZ_EXPAND_MACRO(x) x
+#define HZ_STRINGIFY_MACRO(x) #x
 
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+
+
+
+#include "Hazel/Core/Log.h"
+#include "Hazel/Core/Assert.h"
 
 
 namespace Hazel
--- Hazel/src/Hazel/Core/Log.h
+++ Hazel/src/Hazel/Core/Log.h
@@ -2,8 +2,14 @@
 
 #include "Hazel/Core/Base.h"
 
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
+
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
+
+#pragma warning(pop)
+
 
 namespace Hazel
 {
